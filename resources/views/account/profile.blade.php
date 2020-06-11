c73709ad11f2383024a8e83f02d43b4537d6710e
blessdarah <blessdarahuba@gmail.com>
2020-06-12 00:54:45 +0100

    upadate profile forms on account profile page

--- resources/views/account/profile.blade.php
+++ resources/views/account/profile.blade.php
@@ -11,24 +11,24 @@
         {{-- user defined details --}}
         <div class="card card-body">
             <div class="form-row">
-                <div class="form-group col-6">
+                <div class="form-group col-md-6">
                     <label for="username">Username</label>
                     <input id="username" class="form-control" type="text" name="name" value="{{ Auth::user()->name }}">
                 </div>
 
-                <div class="form-group col-6">
+                <div class="form-group col-md-6">
                     <label for="email">Email address</label>
                     <input id="email" class="form-control" type="email" name="email" value="{{ Auth::user()->email }}">
                 </div>
             </div>
 
             <div class="form-row">
-                <div class="form-group col-6">
+                <div class="form-group col-md-6">
                     <label for="password">New password</label>
                 <input type="password" class="form-control" name="password" id="password" value="{{ Auth::user()->password }}">
                 </div>
 
-                <div class="form-group col-6">
+                <div class="form-group col-md-6">
                     <label for="image">Profile image</label>
                     <input type="file" class="form-control" name="image" id="image">
                 </div>
@@ -44,22 +44,22 @@
         {{-- profile details --}}
         <div class="card card-body my-4">
             <div class="form-row">
-                <div class="form-group col-6">
+                <div class="form-group col-md-6">
                     <label for="facebook">Facebook url</label>
                     <input id="facebook" class="form-control" type="url" name="facebook" value="{{ Auth::user()->profile->facebook ?? '' }}">
                 </div>
 
-                <div class="form-group col-6">
+                <div class="form-group col-md-6">
                     <label for="twitter">Twitter url</label>
                     <input id="twitter" class="form-control" type="url" name="twitter" value="{{ Auth::user()->profile->twitter ?? '' }}">
                 </div>
 
-                <div class="form-group col-6">
+                <div class="form-group col-md-6">
                     <label for="instagram">Instagram url</label>
                     <input id="instagram" class="form-control" type="url" name="instagram" value="{{ Auth::user()->profile->instagram ?? '' }}">
                 </div>
 
-                <div class="form-group col-6">
+                <div class="form-group col-md-6">
                     <label for="whatsapp">Whatsapp number</label>
                     <input id="whatsapp" class="form-control" type="tel" name="whatsapp" value="{{ Auth::user()->profile->whatsapp ?? '' }}">
                 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,57 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <div class="card">
+    <div class="card mb-3">
         <div class="card-header">
-           Dashboard home
-        </div>
-        <div class="card-body">
-            
+           Welcome to your dashboard
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body text-center">
+                    <h3 class="card-title display-2 mb-0">18</h3>
+                    <p><strong>Posts</strong></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body text-center">
+                    <h3 class="card-title display-2 mb-0">20</h3>
+                    <p><strong>Followers</strong></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body text-center">
+                    <h3 class="card-title display-2 mb-0">3</h3>
+                    <p><strong>Albums</strong></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body text-center">
+                    <h3 class="card-title display-2 mb-0">21</h3>
+                    <p><strong>Uploads</strong></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- latest updates --}}
+    <hr>
+    <h3>Recent updates</h3>
+    <ul class="list-group">
+        <a class="list-group-item list-group-item-action" href="#">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim corporis minus voluptates iusto, reprehenderit ducimus ullam nesciunt quaerat non impedit?</p>
+        </a>
+        <a class="list-group-item list-group-item-action" href="#" tabindex="-1" aria-disabled="true">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, possimus?</p>
+        </a>
+    </ul>
 @endsection
